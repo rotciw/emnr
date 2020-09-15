da1a0ec9ad78a1a1e4f3cbeb5220850761910d74
Casper Feng <casp.feng@gmail.com>
2020-09-16 01:04:05 +0200

    add verifyLogin

--- frontend/src/pages/VerifyLogin.tsx
+++ frontend/src/pages/VerifyLogin.tsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import qs from 'qs';
+import { verifyFeideLogin } from '../utils/api';
+import { Redirect } from 'react-router-dom';
 
 interface VerifyLoginProps {}
 
 const VerifyLogin: React.FC<VerifyLoginProps> = () => {
+  useEffect(() => {
+    const verifyLogin = async () => {
+      const queryString = window.location.search;
+      const queryParams = qs.parse(queryString);
+      const code = queryParams['?code'] as string;
+      try {
+        const response = await verifyFeideLogin(code);
+        console.log('logged in');
+      } catch (e) {
+        console.log(e);
+        console.log('login failed');
+      }
+    };
+    verifyLogin();
+  });
   return <div>Hei fra verifylogin</div>;
 };
 
