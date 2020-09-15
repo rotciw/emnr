2dc58be085bb16233582b06cf84ac6a46ee8f75f
Casper Feng <casp.feng@gmail.com>
2020-09-15 23:46:23 +0200

    add api util

---
+++ frontend/src/utils/api.ts
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import { API_URL } from '../config';
+
+//used to retrieve feide auth url
+export const getFeideLogin = () => {
+  return axios
+    .get(API_URL + '/get_login/')
+    .then(function (response) {
+      return response.data;
+    })
+    .catch(function (error) {});
+};
+
+//used to verify successful feide login
+export const verifyFeideLogin = (code: string) => {
+  return axios
+    .get(API_URL + '/verify-token/', {
+      params: {
+        code: code,
+      },
+    })
+    .then(function (response) {
+      localStorage.setItem('token', 'Token ' + response.data.token);
+      axios.defaults.headers.common[
+        'Authorization'
+      ] = `Token ${response.data.token}`;
+      return response.data.token;
+    });
+};
