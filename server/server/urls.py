1ce9e941949486220c1fece53302df610749676c
Haakon Gunleiksrud <haakogun@stud.ntnu.no>
2020-09-22 17:02:45 +0200

    add missing dependencies in backend

--- server/server/urls.py
+++ server/server/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('auth.urls'))
+    path('auth/', include('auth.urls')),
+    path('course/', include('course.urls'))
 ]
