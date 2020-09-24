96284ad608fb23ef8054fc132292fab032d0dc04
Eivind <tutlabrok@gmail.com>
2020-09-24 15:10:59 +0200

    US2 Update course model

--- server/course/models.py
+++ server/course/models.py
@@ -2,5 +2,7 @@ from django.db import models
 
 
 class Course(models.Model):
-    course_code = models.CharField(max_length=200)
+    course_code = models.CharField(max_length=20, primary_key=True)
     course_name = models.CharField(max_length=200)
+    credit = models.FloatField()
+    average_grade = models.FloatField()
