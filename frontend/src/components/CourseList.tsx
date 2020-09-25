fa81607dc95a0d402179ca8ebdfb5ccbaf46fe9f
Haakon Gunleiksrud <haakogun@stud.ntnu.no>
2020-09-25 12:56:18 +0200

    US2 move course list logic from App.stx to CourseList.tsx, and add some logic for fetching data from backend.

--- frontend/src/components/CourseList.tsx
+++ frontend/src/components/CourseList.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Course } from './Course';
+import axios from 'axios';
 
 const Wrapper = styled.div`
   border: 1px solid #ccc;
@@ -13,14 +14,53 @@ const Wrapper = styled.div`
 `;
 
 interface CourseListProps {
+}
+
+interface CourseProps{
   courseNames: Array<String>;
   courseCodes: Array<String>;
+  gradeAvgs: Array<Number>;
+}
+
+const getCourseNames = () => {
+  let courseNames: Array<String> = ['itgk','java','os','progark'];
+  return courseNames;
+}
+
+const getCourseCodes = () => {
+  let courseCodes: Array<String> = ['TDT4110','TDT4100','TDT4186','TDT4240'];
+  return courseCodes;
 }
 
 export const CourseList: React.FC<CourseListProps> = ({
-  courseNames,
-  courseCodes,
   }) => {
+  let courseNames: Array<String> = getCourseNames();
+  let courseCodes: Array<String> = getCourseCodes();
+
+  const getCourses = () => {
+    let courseNames: String[] = [];
+    let courseCodes: String[] = [];
+    let gradeAvgs: Number[] = [];
+  
+    axios
+      .get("http://localhost:8000/course/all/")
+      .then(res => updateCourses({
+        courseNames: res.data.names,
+        courseCodes: res.data.codes,
+        gradeAvgs: res.data.grades 
+      }))
+      .catch(err => console.log(err));
+  
+    let courses: CourseProps = {
+      courseNames,
+      courseCodes,
+      gradeAvgs
+    };
+    
+    return courses;
+  }
+
+  const [courses, updateCourses] = React.useState<CourseProps | null>(getCourses());
 
   return (
     <Wrapper>
@@ -40,7 +80,6 @@ export const CourseList: React.FC<CourseListProps> = ({
               }
           </tbody>
           </table>
-
     </Wrapper>
   );
 };
