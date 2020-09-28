92856578209415036dced465bf092598e93d9bef
Eivind <tutlabrok@gmail.com>
2020-09-28 11:53:40 +0200

    US2 Add endpoint for getting all courses

--- server/course/views.py
+++ server/course/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
+import json
+from course.models import Course
 
 # Create your views here.
 def health(request):
     return HttpResponse("OK")
+
+@api_view(['GET'])
+def get_all_courses(request):
+    return Response(get_courses_from_db())
+
+def get_courses_from_db():
+    data = Course.objects.all()
+    return json.dumps(list(data.values()))
