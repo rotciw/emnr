915d53abb1cfde72b1cf990fa42d554fe4786c9d
Haakon Gunleiksrud <haakogun@stud.ntnu.no>
2020-09-30 09:54:24 +0200

    US2 see an attempt to retrieve data from backend. (NB: Not finished)

--- frontend/src/components/CourseList.tsx
+++ frontend/src/components/CourseList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { Course } from './Course';
 import axios from 'axios';
@@ -22,64 +22,48 @@ interface CourseProps{
   gradeAvgs: Array<Number>;
 }
 
-const getCourseNames = () => {
-  let courseNames: Array<String> = ['itgk','java','os','progark'];
-  return courseNames;
-}
-
-const getCourseCodes = () => {
-  let courseCodes: Array<String> = ['TDT4110','TDT4100','TDT4186','TDT4240'];
-  return courseCodes;
-}
-
 export const CourseList: React.FC<CourseListProps> = ({
   }) => {
-  let courseNames: Array<String> = getCourseNames();
-  let courseCodes: Array<String> = getCourseCodes();
 
-  const getCourses = () => {
-    let courseNames: String[] = [];
-    let courseCodes: String[] = [];
-    let gradeAvgs: Number[] = [];
-  
-    axios
+
+  const [courses, updateCourses] = useState<CourseProps>({courseNames: [''],courseCodes: [''],gradeAvgs: [0]});
+
+  useEffect (() => {
+    const getCourses = async () => {
+    
+      axios
       .get("http://localhost:8000/course/all/")
       .then(res => updateCourses({
-        courseNames: res.data.names,
-        courseCodes: res.data.codes,
-        gradeAvgs: res.data.grades 
-      }))
-      .catch(err => console.log(err));
-  
-    let courses: CourseProps = {
-      courseNames,
-      courseCodes,
-      gradeAvgs
-    };
-    
-    return courses;
-  }
-
-  const [courses, updateCourses] = React.useState<CourseProps | null>(getCourses());
+          courseNames: res.data.course_name,
+          courseCodes: res.data.course_code,
+          gradeAvgs: res.data.average_grade 
+        }))
+        .catch(err => console.log(err));
+        
+      console.log(courses)
+    }
+    getCourses();
+  }, []);
 
   return (
     <Wrapper>
-        <table>
-          <thead>
-              <tr>
-                  <th>Fagnavn</th>
-                  <th>Fagkode</th>
-                  <th>Vurdering</th>
-              </tr>
-          </thead>
-          <tbody>
-              {
-                  courseNames.map(function(currentCourseName, i){
-                      return <Course courseName={currentCourseName} courseCode={courseCodes[i]} score={4.5} key={i} />;
-                  })
-              }
-          </tbody>
-          </table>
+      
+      <table>
+        <thead>
+            <tr>
+                <th>Fagnavn</th>
+                <th>Fagkode</th>
+                <th>Vurdering</th>
+            </tr>
+        </thead>
+        <tbody>
+          {
+            courses.courseNames.map(function(currentCourse, i){
+              return <Course courseName={"Objekt"} courseCode={"TDT4100"} score={4.5} key={i} />;
+            })
+          }
+        </tbody>
+      </table>
     </Wrapper>
   );
 };
