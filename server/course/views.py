0fb2e87be9bd35477964e93efbd6a641d83897e5
haraldgb <haraldgb@gmail.com>
2020-10-01 13:46:29 +0200

    US3 add API endpoint for a single course

--- server/course/views.py
+++ server/course/views.py
@@ -16,3 +16,25 @@ def get_all_courses(request):
 def get_courses_from_db():
     data = Course.objects.all()
     return json.dumps(list(data.values()))
+
+@api_view(['GET'])
+def get_course(request):
+    try:
+        data = get_single_course_from_db(request)
+    except ValueError as e:
+        return Response(str(e), status=400)
+    return Response(data)
+
+def get_single_course_from_db(request):
+    """
+    Helper method for fetching a single course from the database.
+    :param request: GET request containing mandatory parameter code (course code)
+    :raises: ValueError if code is None, or not corresponding to a course in the database
+    :return: A single JSON object, containing the course data.
+    """
+    code = request.GET.get("code", None)
+    if code is None:
+        raise ValueError("No code provided")
+    if not Course.objects.filter(course_code=code).exists():
+        raise ValueError("Course does not exist in database")
+    return Course.objects.filter(course_code=code).values()[0]
