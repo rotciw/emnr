9098fc91fe963ec1e5a8d53c0d94fdf16cedb43d
Eivind <tutlabrok@gmail.com>
2020-10-01 11:45:10 +0200

    US2 Add unit test for course endpoint

--- server/course/tests.py
+++ server/course/tests.py
@@ -3,6 +3,8 @@ import json
 from course.models import Course
 from course.views import get_all_courses, get_courses_from_db
 from django.test.client import RequestFactory
+from django.test import Client
+
 
 # Create your tests here.
 class GetAllCoursesTest(TestCase):
@@ -21,14 +23,18 @@ class GetAllCoursesTest(TestCase):
 			data = json.load(f)
 		courses = []
 		for course_dict in data:
-			courses.append(Course.create(code=course_dict["course_code"], name=course_dict["course_name"], credit=course_dict["credit"],
-						  average_grade=course_dict["average_grade"]))
+			courses.append(Course.create(code=course_dict["course_code"], name=course_dict["course_name"],
+										 credit=course_dict["credit"],
+										 average_grade=course_dict["average_grade"]))
 		return courses
 
 	def test_get_courses_from_db_no_parameters(self):
-		mock_request = self.rf.get("/courses/all/")
+		mock_request = self.rf.get("/course/all/")
 		data = get_courses_from_db(mock_request)
-		self.assertEqual(len(data), len(self._create_models_without_saving()))
+		source_data = self._create_models_without_saving()
+		self.assertEqual(len(data), len(source_data))
+		for i in range(len(data)):
+			self.assertEqual(data[i]["course_code"], source_data[i].course_code)
 
 	def test_get_courses_from_db_normal_parameters(self):
 		pass
@@ -37,7 +43,13 @@ class GetAllCoursesTest(TestCase):
 		pass
 
 	def test_get_all_courses_no_parameters(self):
-		pass
+		c = Client()
+		response = c.get("/course/all/")
+		source_data = self._create_models_without_saving()
+		self.assertEqual(len(response.data), len(source_data))
+		for i in range(len(response.data)):
+			self.assertEqual(response.data[i]["course_code"], source_data[i].course_code)
+
 
 	def test_get_all_courses_normal_parameters(self):
 		pass
