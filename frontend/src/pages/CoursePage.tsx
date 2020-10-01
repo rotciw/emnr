411a984771a0ae755d8c6d805a67244fdbbc672b
Wictor <wictor_x@hotmail.com>
2020-10-01 15:39:35 +0200

    Add useEffect hook to fetch course information

--- frontend/src/pages/CoursePage.tsx
+++ frontend/src/pages/CoursePage.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
 import { Layout } from 'styles/Layout';
 import {
@@ -11,6 +11,7 @@ import { BoldTitle, Title, SubTitle, GoBackText } from 'styles/Text';
 import { RateCourseButton } from 'styles/Buttons';
 import { Circle, RotatedSquare } from 'styles/Shapes';
 import { defaultTheme } from 'styles/theme';
+import axios from 'axios';
 
 interface CourseViewProps {
   courseName: String;
@@ -22,28 +23,49 @@ interface CourseViewProps {
 export const CoursePage: React.FC<CourseViewProps> = (
   props: CourseViewProps,
 ) => {
-  const code: string = useLocation().pathname.substr(8);
-  const { courseCode, courseName, score } = props.location.state;
+  const courseCode: string = useLocation().pathname.substr(8);
+  const [courseInfo, setCourseInfo] = useState<any>({});
 
   const history = useHistory();
-  const handleOnClick = useCallback(() => history.push('/'), [history]);
+  const handleBackClick = useCallback(() => history.push('/'), [history]);
+
+  useEffect(() => {
+    const getCourses = async () => {
+      await axios
+        .get('http://localhost:8000/course/?code=' + courseCode)
+        .then(res => setCourseInfo(res.data))
+        .catch((err) => console.log(err));
+    };
+    getCourses();
+  }, []);
 
   return (
-    <Layout padding="0 20%">
+    <Layout padding='0 20%'>
       <FlexContainer>
         <FlexItem margin='0 0 0 10vh'>
-          <FlexItem margin='2vh 0 4vh 0' onClick={handleOnClick}>
+          <FlexItem margin='2vh 0 4vh 0' onClick={handleBackClick}>
             <GoBackText>Tilbake</GoBackText>
           </FlexItem>
-          <Title margin='0 0 5px 0'>{courseCode}</Title>
-          <BoldTitle fontSize='30px'>{courseName}</BoldTitle>
-          <BoldTitle margin='10px 0 0 0'>{score} / 5</BoldTitle>
+          <Title margin='0 0 5px 0'>{courseInfo.course_code}</Title>
+          <BoldTitle fontSize='30px'>{courseInfo.course_name}</BoldTitle>
+          <BoldTitle margin='10px 0 0 0'>{courseInfo.score} / 5</BoldTitle>
           <SubTitle margin='0 0 4vh 0'>Basert på x antall vurderinger</SubTitle>
           <RateCourseButton>Vurder {courseCode}</RateCourseButton>
         </FlexItem>
         <LocalShapeContainer>
-          <Circle color={defaultTheme.lightBlue} size='215px' left='0' top='50px' />
-          <RotatedSquare color={defaultTheme.blue} size='150px' left='100px' top='30px' angle='20deg' />
+          <Circle
+            color={defaultTheme.lightBlue}
+            size='215px'
+            left='0'
+            top='50px'
+          />
+          <RotatedSquare
+            color={defaultTheme.blue}
+            size='150px'
+            left='100px'
+            top='30px'
+            angle='20deg'
+          />
         </LocalShapeContainer>
       </FlexContainer>
       <HrLine />
