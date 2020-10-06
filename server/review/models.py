0c1105311cbaea3aec2cf8fcc45fbbc2d5880d1c
Sylvi Huynh <sylvi2602@hotmail.com>
2020-10-06 13:58:13 +0200

    US4 create review model

--- server/review/models.py
+++ server/review/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from course.models import Course
+
 
 # Create your models here.
+
+class Review(models.Model):
+    user_email = models.EmailField(max_length=254)
+    course_code = models.ForeignKey(Course, on_delete=models.CASCADE)
+    score = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    workload = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    difficulty = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    review_text = models.TextField()
+    full_name = models.CharField(max_length=100)
+    study_programme= models.CharField(max_length=10)
+
+
+
+
--- server/review/views.py
+++ server/review/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
+@api_view(['POST'])
+def post_reviews(request):
+	try:
+		data = get_courses_from_db(request)
+	except ValueError as e:
+		return Response(str(e), status=400)
+	return Response(data)
+
+
+def validate_and_save_review(request):
+
