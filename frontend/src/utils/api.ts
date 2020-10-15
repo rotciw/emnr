7d56cc0868871a993df7b97f78cbda12c88d0516
Haakon Gunleiksrud <haakogun@stud.ntnu.no>
2020-10-15 09:04:42 +0200

    US4 see more effort towards posting reviews.

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -30,14 +30,14 @@ export const verifyFeideLogin = (code: string) => {
 };
 
 //used to verify if token is valid
-/*export const hasToken = () => {
+export const hasToken = () => {
   if (!localStorage.getItem('token')) {
     return false;
   }
   return axios.get(API_URL + 'auth/validate_token/').then(function (response) {
     return response.data === 'False';
   });
-};*/
+};
 
 //fetch local token
 export const getLocalToken = () => {
