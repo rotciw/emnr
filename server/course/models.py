3b7d9432a1bd17cea33ed8fca38c5cc797ca20e5
haraldgb <haraldgb@gmail.com>
2020-10-22 11:04:26 +0200

    US10 add unfinished implementations of pass_rate

--- server/course/models.py
+++ server/course/models.py
@@ -6,8 +6,10 @@ class Course(models.Model):
     course_name = models.CharField(max_length=200)
     credit = models.FloatField()
     average_grade = models.FloatField()
+    pass_rate = models.FloatField()
 
     @classmethod
-    def create(cls, code, name, credit, average_grade):
-        course = cls(course_code=code, course_name=name, credit=credit, average_grade=average_grade)
+    def create(cls, code, name, credit, average_grade, pass_rate):
+        course = cls(course_code=code, course_name=name, credit=credit, average_grade=average_grade,
+                     pass_rate=pass_rate)
         return course
