0ea1ad4f3fa3e77628539129aa40933c17099f50
haraldgb <haraldgb@gmail.com>
2020-10-22 11:28:40 +0200

    US10 add pass_rate as valid order_by parameter

--- server/course/views.py
+++ server/course/views.py
@@ -57,7 +57,7 @@ def get_courses_from_db(request):
 
     # Get and validate order_by and ascending parameters
     order_by = request.GET.get("order_by", "course_name")
-    valid_order_parameters = ["course_code", "course_name", "credit", "average_grade", "review_count"]
+    valid_order_parameters = ["course_code", "course_name", "credit", "average_grade", "review_count", "pass_rate"]
     if order_by not in valid_order_parameters:
         raise ValueError(
             "Invalid value for order_by: {}. Valid  values: {}".format(order_by, valid_order_parameters))
