c649d58b8772199755f68c8cb0342dc3a3b9bb79
Eivind <tutlabrok@gmail.com>
2020-10-22 14:38:50 +0200

    Add migration for removal of review validators

--- server/review/models.py
+++ server/review/models.py
@@ -5,9 +5,9 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class Review(models.Model):
     user_email = models.EmailField(max_length=254, blank=False)
     course_code = models.CharField(max_length=20, blank=False)
-    score = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)], blank=False)
-    workload = models.IntegerField(validators=[MinValueValidator(-1), MaxValueValidator(2)], blank=False)
-    difficulty = models.IntegerField(validators=[MinValueValidator(-1), MaxValueValidator(2)], blank=False)
+    score = models.IntegerField(blank=False)
+    workload = models.IntegerField(blank=False)
+    difficulty = models.IntegerField(blank=False)
     review_text = models.TextField()
     full_name = models.CharField(max_length=100, blank=False)
     study_programme = models.CharField(max_length=20, blank=False)
