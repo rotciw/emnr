30295bc23ae0ebfc442c37950f4b66ed49dccc1d
Wictor <wictor_x@hotmail.com>
2020-10-30 10:32:22 +0100

    revert boldtitles to dev equivalent

--- frontend/src/pages/CoursePage.tsx
+++ frontend/src/pages/CoursePage.tsx
@@ -118,8 +118,12 @@ const CoursePage: React.FC<CourseViewProps> = (props: CourseViewProps) => {
           <MobileFlexContainer>
             <FlexItem margin='0 5vw 3vh 0'>
               <Title margin='0 0 5px 0'>{courseInfo.course_code}</Title>
-              <BoldTitle fontSize='30px'>{courseInfo.course_name}</BoldTitle>
-              <BoldTitle margin='10px 0 0 0'>
+              <BoldTitle>{courseInfo.course_name}</BoldTitle>
+              <BoldTitle
+                fontSize='50px'
+                mobileFontSize='40px'
+                margin='10px 0 0 0'
+              >
                 {scoreAvg.toFixed(1)} / 5
               </BoldTitle>
               <SubTitle margin='0 0 4vh 0'>
