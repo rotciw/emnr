a2063cb701a2fdfb003c396d767d0cf3f59fe9c0
Jakob Kielland <jakob-ek@hotmail.com>
2020-11-05 12:53:35 +0100

    split radio button bar component into two different ones, one for sort form and one for review form

--- frontend/src/components/RadioButtonBar.tsx
+++ frontend/src/components/RadioButtonBar.tsx
@@ -4,14 +4,24 @@ import { FlexContainer } from 'styles/Containers';
 
 interface RadioButtonProps {
   radioID: string;
-  defaultChecked: number; // Number between 0 and 5. 0 Means no button is checked by default.
+  valueVariable: number; // Number between 0 and 5. 0 Means no button is checked by default.
   valueSetter: (value: number) => void;
 }
 
-const MarginLabel = styled.label`
+const MarginNumberLabel = styled.label`
   margin-right: 16px;
   line-height: 20px;
   font-family: gilroymedium;
+`;
+
+const MarginTextLabel = styled(MarginNumberLabel)`
+  margin: 0 5px 0 0;
+  font-family: gilroylight;
+  font-size: 0.8em;
+`;
+
+const MirrorMarginTextLabel = styled(MarginTextLabel)`
+  text-align: right;
 `;
 
 const RadioButton = styled.input`
@@ -19,9 +29,14 @@ const RadioButton = styled.input`
   margin-right: 10px;
 `;
 
-export const RadioButtonsBar: React.FC<RadioButtonProps> = ({
+const RadioButtonContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+// Used in review form, does not use variable to temporarily store value, just the radio button element itself
+export const ScoreRadioButtonsBar: React.FC<RadioButtonProps> = ({
   radioID,
-  defaultChecked,
   valueSetter,
 }) => {
   return (
@@ -32,47 +47,86 @@ export const RadioButtonsBar: React.FC<RadioButtonProps> = ({
         name={radioID}
         value={1}
         onChange={() => valueSetter(1)}
-        checked={defaultChecked === 1}
       />
-      <MarginLabel htmlFor={radioID + '1'}>1</MarginLabel>
+      <MarginNumberLabel htmlFor={radioID + '1'}>1</MarginNumberLabel>
       <RadioButton
         type='radio'
         id={`${radioID}2`}
         name={radioID}
         value={2}
         onChange={() => valueSetter(2)}
-        checked={defaultChecked === 2}
       />
-      <MarginLabel htmlFor={radioID + '2'}>2</MarginLabel>
+      <MarginNumberLabel htmlFor={radioID + '2'}>2</MarginNumberLabel>
       <RadioButton
         type='radio'
         id={`${radioID}3`}
         name={radioID}
         value={3}
         onChange={() => valueSetter(3)}
-        checked={defaultChecked === 3}
       />
-      <MarginLabel htmlFor={radioID + '3'}>3</MarginLabel>
+      <MarginNumberLabel htmlFor={radioID + '3'}>3</MarginNumberLabel>
       <RadioButton
         type='radio'
         id={`${radioID}4`}
         name={radioID}
         value={4}
         onChange={() => valueSetter(4)}
-        checked={defaultChecked === 4}
       />
-      <MarginLabel htmlFor={radioID + '4'}>4</MarginLabel>
+      <MarginNumberLabel htmlFor={radioID + '4'}>4</MarginNumberLabel>
       <RadioButton
         type='radio'
         id={`${radioID}5`}
         name={radioID}
         value={5}
         onChange={() => valueSetter(5)}
-        checked={defaultChecked === 5}
       />
-      <MarginLabel htmlFor={`${radioID}5`}>5</MarginLabel>
+      <MarginNumberLabel htmlFor={`${radioID}5`}>5</MarginNumberLabel>
     </FlexContainer>
   );
 };
 
-export default RadioButtonsBar;
+// Used for advanced sorting interface, valueVariable must be updated whenever a radiobutton is clicked
+export const WeightRadioButtonsBar: React.FC<RadioButtonProps> = ({
+  radioID,
+  valueVariable,
+  valueSetter,
+}) => {
+  return (
+    <FlexContainer>
+      <MirrorMarginTextLabel htmlFor={radioID + '0'}>Ikke viktig</MirrorMarginTextLabel>
+      <RadioButton
+        type='radio'
+        id={`${radioID}0`}
+        name={radioID}
+        value={0}
+        onChange={() => valueSetter(0)}
+        checked={valueVariable === 0}
+      />
+      <RadioButton
+        type='radio'
+        id={`${radioID}1`}
+        name={radioID}
+        value={1}
+        onChange={() => valueSetter(1)}
+        checked={valueVariable === 1}
+      />
+      <RadioButton
+        type='radio'
+        id={`${radioID}2`}
+        name={radioID}
+        value={2}
+        onChange={() => valueSetter(2)}
+        checked={valueVariable === 2}
+      />
+      <RadioButton
+        type='radio'
+        id={`${radioID}3`}
+        name={radioID}
+        value={3}
+        onChange={() => valueSetter(3)}
+        checked={valueVariable === 3}
+      />
+      <MarginTextLabel htmlFor={`${radioID}3`}>Veldig viktig</MarginTextLabel>
+    </FlexContainer>
+  );
+};
