2540f07e232e80db844826a86a056311b392e9d8
Eivind Furdal <tutlabrok@gmail.com>
2020-11-06 11:41:59 +0100

    US12 Add test file for user deletion

--- server/user/tests.py
+++ server/user/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class DeleteUserTest(TestCase):
+
+    def test_delete_user_no_token(self):
+        pass
+
+    def test_delete_user_invalid_token(self):
+        pass
+
+    def test_delete_user_not_admin(self):
+        pass
+
+    def test_delete_user_no_email(self):
+        pass
+
+    def test_delete_user_already_banned(self):
+        pass
+
+    def test_delete_user_remaining_reviews_for_all_courses(self):
+        # Check that is updated, and that
+        pass
+
+    def test_delete_user_no_remaining_reviews_for_courses(self):
+        pass
